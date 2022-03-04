da95200100ef49dd10766c1dc6864cce5484b9db
Maksym <cheestick@gmail.com>
2022-03-04 16:53:49 +0200

    refactor fetch and render functions

--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -4,14 +4,17 @@ const refs = {
   cardContainer: document.querySelector('.js-card-container'),
 };
 
-fetch('https://pokeapi.co/api/v2/pokemon/2')
-  .then(response => response.json())
+fetchPokemon()
+  .then(renderPokemonCard)
+  .catch(error => console.log(error));
+
+function fetchPokemon() {
+  return fetch('https://pokeapi.co/api/v2/pokemon/2').then(response => response.json());
   // .then(pokemon => {
   //   console.log(pokemon);
   //   renderPokemonCard(pokemon);
   // })
-  .then(renderPokemonCard)
-  .catch(error => console.log(error));
+}
 
 function renderPokemonCard(pokemon) {
   const markup = pokemonCardTml(pokemon);
