c88a8036820e7be1c93d11eabe832ec8a3019562
Maksym <cheestick@gmail.com>
2022-03-05 00:06:49 +0200

    fetch authentication

--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -36,3 +36,24 @@ function onFetchError(error) {
 
 fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200');
 fetch('https://pixabay.com/api/?key=4823621-792051e21e56534e6ae2e472f&q=cat&image_type=photo');
+fetch(
+  'https://api.weatherstack.com/current?access_key=addb9340d3bd931230aeb471899ebea6&query=39.9042,116.4074',
+);
+
+const url = 'https://newsapi.org/v2/everything?q=cars';
+const options = {
+  headers: {
+    'X-Api-Key': '4330ebfabc654a6992c2aa792f3173a3',
+  },
+};
+
+fetch(url, options);
+
+const urlDogs = 'https://newsapi.org/v2/everything?q=dogs';
+const opt = {
+  headers: {
+    Authorization: '4330ebfabc654a6992c2aa792f3173a3',
+  },
+};
+
+fetch(urlDogs, opt);
