2ef6cf16afaad395d4df58a566e961eb9d01778c
Maksym <cheestick@gmail.com>
2022-03-04 23:33:22 +0200

    fetch request parameters

--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -31,3 +31,8 @@ function onFetchError(error) {
   console.log(error);
   alert('Ooops! Something gone wrong.');
 }
+
+//request parameters: pokemon?limit=100&offset=200
+
+fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200');
+fetch('https://pixabay.com/api/?key=4823621-792051e21e56534e6ae2e472f&q=cat&image_type=photo');
