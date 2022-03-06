0ac283f5118e911ba26169d16b4019062402cba0
Maksym <cheestick@gmail.com>
2022-03-06 17:11:56 +0200

    rest api pagination files inited

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './sass/main.scss';
+
+const refs = {
+  searchForm: document.querySelector('.js-search-form'),
+  articlesContainer: document.querySelector('.js-articles-container'),
+};
+
+const options = {
+  headers: {
+    Authorization: '4c849de3a17848fba6b5da568b220f90',
+  },
+};
+
+const url = 'https://newsapi.org/v2/everything?q=cat&language=en&pageSize=5&page=1';
+
+fetch(url, options)
+  .then(r => r.json())
+  .then(console.log);
+// fetch('https://newsapi.org/v2/everything?q=cat&language=en&pageSize=5&page=2', options)
+//   .then(r => r.json())
+//   .then(console.log);
