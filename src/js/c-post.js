ccda03793cd9bf07d7ef189324e25bbb4caea074
Maksym <cheestick@gmail.com>
2022-03-07 22:00:09 +0200

    c-post completed

---
+++ src/js/c-post.js
@@ -0,0 +1,42 @@
+const BASE_URL = 'http://localhost:4040';
+
+const newBook = {
+  title: 'Will of Ukraine',
+  author: 'Maksym Chystiakov',
+  genres: ['memories'],
+  rating: 9.89,
+};
+
+function addBook(book) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(book),
+  };
+
+  return fetch(`${BASE_URL}/books`, options).then(res => res.json());
+}
+
+addBook(newBook).then(renderBook);
+
+addBook({
+  title: 'Pray of Ukraine',
+  author: 'Maksym Chystiakov',
+  genres: ['philosofy'],
+  rating: 9.0,
+}).then(renderBook);
+
+addBook({
+  title: 'Ukraine. The Liberty',
+  author: 'Maksym Chystiakov',
+  genres: ['politics'],
+  rating: 8.5,
+}).then(renderBook);
+
+function renderBook(book) {
+  console.log('Becked requested. lets draw an interface');
+  console.log(book);
+}
