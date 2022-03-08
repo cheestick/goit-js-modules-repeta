47d0c741f4569421c2dbea5c8aad5269bddbd878
Maksym <cheestick@gmail.com>
2022-03-08 13:45:28 +0200

    crud completed

--- src/index.js
+++ src/index.js
@@ -3,4 +3,20 @@ import './sass/main.scss';
 // import './js/r-get';
 // import './js/c-post';
 // import './js/u-patch';
-import './js/d-delete';
+// import './js/d-delete';
+
+// // difference between callback and function call
+// function add(a, b) {
+//   return a + b;
+// }
+
+// console.log(add);
+
+// // check 404 status
+// fetch('').then(response => {
+//   if (response.ok) {
+//     return response.json();
+//   }
+
+//   throw new Error(response.statusText);
+// });
