155074fa5327d8d667a2e528ae875f2c727590c1
cheestick <74751903+cheestick@users.noreply.github.com>
2022-03-09 18:36:58 +0000

    Deploying to gh-pages from @ cheestick/goit-js-modules-repeta@3e1e88c064a2db61d0820cd7fb3575edc1933de4 🚀

--- src.14b97b53.js
+++ src.14b97b53.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-modules-repeta/src.4f0dfa62.js.map
+//# sourceMappingURL=/goit-js-modules-repeta/src.14b97b53.js.map
