ba9f1c638824a6523999cde29719369ab253fedc
Maksym <cheestick@gmail.com>
2022-03-09 10:42:43 +0200

    tabs click acted

--- src/js/tabs-plugin.js
+++ src/js/tabs-plugin.js
@@ -3,4 +3,27 @@ const refs = {
   panes: document.querySelector(`#tabs-1 [data-panes]`),
 };
 
-console.log(refs);
+refs.controls.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'A') {
+    console.log('This is not a reference');
+    return;
+  }
+
+  const currentActiveControlItem = refs.controls.querySelector('.controls__item--active');
+
+  if (currentActiveControlItem) {
+    currentActiveControlItem.classList.remove('controls__item--active');
+    const paneId = currentActiveControlItem.getAttribute('href').slice(1);
+    const pane = refs.panes.querySelector(`#${paneId}`);
+    pane.classList.remove('pane--active');
+  }
+
+  const controlItem = event.target;
+  controlItem.classList.add('controls__item--active');
+
+  const paneId = controlItem.getAttribute('href').slice(1);
+
+  const pane = refs.panes.querySelector(`#${paneId}`);
+  pane.classList.add('pane--active');
+});
