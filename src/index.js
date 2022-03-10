5a5a4e1cf9c6f46a23d53252ff5faabf2f9f50ab
Maksym <cheestick@gmail.com>
2022-03-10 13:29:30 +0200

    charts statrted

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
-import './sass/main.scss';
+// import './sass/main.scss';
+// import './ZonAnn.Ts+dSST.csv';
+
+const ctx = document.querySelector('.js-chart').getContext('2d');
+console.log(ctx);
+
+const myChart = new Chart(ctx, {
+  type: 'bubble',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+// async function fetchData() {
+//   const response = await fetch('./ZonAnn.Ts+dSST.csv');
+//   return await response.text();
+// }
+
+// fetchData().then(data => {
+//   console.log(Papa.parse(data, { header: true }).data);
+// });
