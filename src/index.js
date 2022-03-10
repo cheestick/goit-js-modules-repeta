99399b5509275337e9cf8100d2490fe3d108a6d9
Maksym <cheestick@gmail.com>
2022-03-10 20:22:27 +0200

    charts refactored

--- src/index.js
+++ src/index.js
@@ -1,51 +1,63 @@
 // import './sass/main.scss';
 // import './ZonAnn.Ts+dSST.csv';
 
+const GLOBAL_MEAN_TEMPERATURE = 14;
+
 const ctx = document.querySelector('.js-chart').getContext('2d');
 console.log(ctx);
 
-const myChart = new Chart(ctx, {
-  type: 'bubble',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
+fetchData()
+  .then(parseData)
+  .then(getLabelsAndData)
+  .then(({ years, temps }) => drawChart(years, temps));
+
+async function fetchData() {
+  const response = await fetch('./ZonAnn.Ts+dSST.csv');
+  return await response.text();
+}
+
+function parseData(data) {
+  return Papa.parse(data, { header: true }).data;
+}
+
+function getLabelsAndData(data) {
+  return data.reduce(
+    (acc, entry) => {
+      acc.years.push(entry.Year);
+      acc.temps.push(GLOBAL_MEAN_TEMPERATURE + Number(entry.Glob));
+      return acc;
+    },
+    { years: [], temps: [] },
+  );
+}
+
+function drawChart(labels, data) {
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: '# Average Global Temperature',
+          data: data,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1,
+          fill: true,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: false,
+          ticks: {
+            callback(value, index, values) {
+              return value + '°C';
+            },
+          },
+        },
       },
     },
-  },
-});
-
-// async function fetchData() {
-//   const response = await fetch('./ZonAnn.Ts+dSST.csv');
-//   return await response.text();
-// }
-
-// fetchData().then(data => {
-//   console.log(Papa.parse(data, { header: true }).data);
-// });
+  });
+}
